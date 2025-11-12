d204a039447de5a27ed2f4d66f0312ac36babdcb
FilippeZ <143500696+FilippeZ@users.noreply.github.com>
2025-11-12 11:18:30 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'; // Or vue(), etc.
 
 export default defineConfig({
   plugins: [react()], // 
-  base: "/Filippos-ParaskevasZygouris/",
+  base: "/Filippos-ParaskevasZygouris",
 });
